6ea36c13331a17cdd0fe61fcdf890a712acfbb26
Julio Noguera <noguerajulio24@gmail.com>
2020-06-04 21:07:18 -0500

    json-server

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -2,7 +2,47 @@ import React from 'react';
 
 const EditarProducto = () => {
     return(
-        <h1>Editar Producto</h1>
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Editar nuevo producto
+                        </h2>
+
+                        <from>
+                            <div className="form-group">
+                                <label>Nombre </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre Producto"
+                                    name="nombre"
+                                >
+                                </input>
+                            </div>
+                            <div className="form-group">
+                                <label>Precio Producto </label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Precio Producto"
+                                    name="precio"
+                                >
+                                </input>
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >
+                                Editar Producto
+                            </button>
+                        </from>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
