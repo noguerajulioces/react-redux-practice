e747f3f0f4a03078572800913a53b431db126d61
Julio Noguera <noguerajulio24@gmail.com>
2020-06-04 20:20:08 -0500

    editar product

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Productos from './components/Productos';
 import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
 
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Productos}></Route>
           <Route exact path="/productos/nuevo" component={NuevoProducto}></Route>
+          <Route exact path="/productos/editar/:id" component={NuevoProducto}></Route>
         </Switch>
       </div>
     </Router>
